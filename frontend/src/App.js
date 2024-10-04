e285b4201f9b60525726c8d80c93dbed8f9304e7
simmithapad <simmithapad23@gmail.com>
2024-10-04 22:48:42 +0530

    lander fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Tiles from './components/Tiles/Tiles.jsx'; 
-import Page1 from './pages/About.jsx';
-// import Page2 from './pages/Page2';
+import Page1 from './components/Header/Header.jsx';
+import Page2 from './components/AdventureSports/Home.jsx';
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Tiles />} />
-          <Route path="/page1" element={<Page1 />} />
-          {/* <Route path="/page2" element={<Page2 />} /> */}
+          <Route path="/dailysports" element={<Page1 />} />
+          <Route path="/adventuresports" element={<Page2 />} />
         </Routes>
       </div>
     </Router>
